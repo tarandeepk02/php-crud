9842dc138bd1a2974df98e17b5aac2a461ed5004
The-Tarandeep-Kaur <115900559+tarandeepk02@users.noreply.github.com>
2025-03-28 15:51:40 -0700

    Update and rename users.sql to records.sql

--- records.sql
+++ records.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 28, 2025 at 11:28 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
